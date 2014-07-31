84a57ede2ce7d6f45504d3b4ee0517e0da465d81
Gunnar Hilling <gunnar@hilling.de>
2014-07-31 09:01:17 +0200

    enhance dependency management.

--- cdi-test-core/src/main/java/de/hilling/junit/cdi/scope/Mockable.java
+++ cdi-test-core/src/main/java/de/hilling/junit/cdi/scope/Mockable.java
@@ -1,12 +1,14 @@
 package de.hilling.junit.cdi.scope;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.interceptor.InterceptorBinding;
 
+@Inherited
 @InterceptorBinding
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)

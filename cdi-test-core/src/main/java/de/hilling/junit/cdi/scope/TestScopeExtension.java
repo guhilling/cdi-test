4ee2c9f4ac5844d780e4e094e16a5dcc2c499350
Gunnar Hilling <gunnar@hilling.de>
2015-03-17 14:06:50 +0100

    CDIT-3 Enhance Javadoc for base api.

--- cdi-test-core/src/main/java/de/hilling/junit/cdi/scope/TestScopeExtension.java
+++ cdi-test-core/src/main/java/de/hilling/junit/cdi/scope/TestScopeExtension.java
@@ -9,13 +9,26 @@ import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * CDI {@link javax.enterprise.inject.spi.Extension} to enable proxying of (nearly) all method
+ * invocations.
+ * <p>
+ *     By default, these are all classes, except:
+ *     <ul>
+ *         <li>Anonymous classes.</li>
+ *         <li>Enums.</li>
+ *     </ul>
+ *     To preventing <em>everything</em> from being proxied it is possible to define explicit
+ *     packages.
+ * </p>
+ */
 public class TestScopeExtension implements Extension, Serializable {
     private static final long serialVersionUID = 1L;
     private static final Logger LOG = Logger.getLogger(TestScopeExtension.class
             .getCanonicalName());
 
     /**
-     * add contexts after bean discovery.
+     * Add contexts after bean discovery.
      *
      * @param afterBeanDiscovery
      * @param beanManager
@@ -23,19 +36,15 @@ public class TestScopeExtension implements Extension, Serializable {
     public void afterBeanDiscovery(
             @Observes AfterBeanDiscovery afterBeanDiscovery,
             BeanManager beanManager) {
-        addContexts(afterBeanDiscovery);
-    }
-
-    private void addContexts(AfterBeanDiscovery abd) {
-        abd.addContext(new TestSuiteContext());
-        abd.addContext(new TestContext());
+        afterBeanDiscovery.addContext(new TestSuiteContext());
+        afterBeanDiscovery.addContext(new TestContext());
     }
 
     public <X> void processBean(@Observes ProcessAnnotatedType<X> pat) {
         AnnotatedType<X> type = pat.getAnnotatedType();
         Class<X> javaClass = type.getJavaClass();
         if (shouldProxyCdiType(javaClass)) {
-            AnnotatedTypeBuilder<X> builder = new AnnotatedTypeBuilder<X>();
+            AnnotatedTypeBuilder<X> builder = new AnnotatedTypeBuilder<>();
             builder.readFromType(type);
             builder.addToClass(new AnnotationLiteral<Mockable>() {
                 private static final long serialVersionUID = 1L;
@@ -55,10 +64,7 @@ public class TestScopeExtension implements Extension, Serializable {
         if(!javaClass.getName().startsWith("de.hilling")) {
             return false;
         }
-        if(javaClass.isEnum()) {
-            return false;
-        }
-        return true;
+        return !javaClass.isEnum();
     }
 
 }

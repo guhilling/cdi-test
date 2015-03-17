342493a091f1150c94132723d6aa3411d64d3d23
Gunnar Hilling <gunnar@hilling.de>
2015-03-17 10:25:09 +0100

    update issue management.

--- cdi-test-core/src/test/java/de/hilling/junit/cdi/CdiTestAbstract.java
+++ cdi-test-core/src/test/java/de/hilling/junit/cdi/CdiTestAbstract.java
@@ -2,10 +2,15 @@ package de.hilling.junit.cdi;
 
 import org.junit.runner.RunWith;
 
+import javax.annotation.PostConstruct;
 import java.util.logging.Logger;
 
 @RunWith(CdiUnitRunner.class)
 public abstract class CdiTestAbstract {
     protected static final Logger LOG = Logger.getLogger(CdiTestAbstract.class.getName());
 
+    @PostConstruct
+    public void init() {
+        System.out.println("created");
+    }
 }

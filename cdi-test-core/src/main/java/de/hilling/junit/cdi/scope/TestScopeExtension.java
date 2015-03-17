224bd84dced1990023fbe5f179bac72dbaa8e31a
Gunnar Hilling <gunnar@hilling.de>
2015-03-17 17:25:04 +0100

    CDIT-4 Make proxy packages configurable

--- cdi-test-core/src/main/java/de/hilling/junit/cdi/scope/TestScopeExtension.java
+++ cdi-test-core/src/main/java/de/hilling/junit/cdi/scope/TestScopeExtension.java
@@ -1,6 +1,7 @@
 package de.hilling.junit.cdi.scope;
 
 import org.apache.deltaspike.core.util.metadata.builder.AnnotatedTypeBuilder;
+import org.junit.runner.RunWith;
 
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.*;
@@ -10,17 +11,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * CDI {@link javax.enterprise.inject.spi.Extension} to enable proxying of (nearly) all method
- * invocations.
- * <p>
- *     By default, these are all classes, except:
- *     <ul>
- *         <li>Anonymous classes.</li>
- *         <li>Enums.</li>
- *     </ul>
- *     To preventing <em>everything</em> from being proxied it is possible to define explicit
- *     packages.
- * </p>
+ * CDI {@link javax.enterprise.inject.spi.Extension} to enable proxying of (nearly) all method invocations. <p> By
+ * default, these are all classes, except: <ul> <li>Anonymous classes.</li> <li>Enums.</li> </ul> To preventing
+ * <em>everything</em> from being proxied it is possible to define explicit packages. </p>
  */
 public class TestScopeExtension implements Extension, Serializable {
     private static final long serialVersionUID = 1L;
@@ -43,7 +36,18 @@ public class TestScopeExtension implements Extension, Serializable {
     public <X> void processBean(@Observes ProcessAnnotatedType<X> pat) {
         AnnotatedType<X> type = pat.getAnnotatedType();
         Class<X> javaClass = type.getJavaClass();
-        if (shouldProxyCdiType(javaClass)) {
+        if (isTestClass(javaClass)) {
+            AnnotatedTypeBuilder<X> builder = new AnnotatedTypeBuilder<>();
+            builder.readFromType(type);
+            builder.addToClass(new AnnotationLiteral<TestSuiteScoped>() {
+                private static final long serialVersionUID = 1L;
+            });
+            try {
+                pat.setAnnotatedType(builder.create());
+            } catch (RuntimeException e) {
+                LOG.log(Level.SEVERE, "unable to process type " + pat, e);
+            }
+        } else if (shouldProxyCdiType(javaClass)) {
             AnnotatedTypeBuilder<X> builder = new AnnotatedTypeBuilder<>();
             builder.readFromType(type);
             builder.addToClass(new AnnotationLiteral<Mockable>() {
@@ -57,11 +61,15 @@ public class TestScopeExtension implements Extension, Serializable {
         }
     }
 
+    private <X> boolean isTestClass(Class<X> javaClass) {
+        return javaClass.isAnnotationPresent(RunWith.class);
+    }
+
     private <X> boolean shouldProxyCdiType(Class<X> javaClass) {
-        if(javaClass.isAnonymousClass()) {
+        if (javaClass.isAnonymousClass()) {
             return false;
         }
-        if(!javaClass.getName().startsWith("de.hilling")) {
+        if (!javaClass.getName().startsWith("de.hilling")) {
             return false;
         }
         return !javaClass.isEnum();

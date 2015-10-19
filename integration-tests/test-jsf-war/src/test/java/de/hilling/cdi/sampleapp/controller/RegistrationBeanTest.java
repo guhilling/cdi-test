6389e0f577c19709cdf30923cae321717f70c957
Gunnar Hilling <gunnar@hilling.de>
2015-10-19 23:35:30 +0200

    Add unit tests.

--- integration-tests/test-jsf-war/src/test/java/de/hilling/cdi/sampleapp/controller/RegistrationBeanTest.java
+++ integration-tests/test-jsf-war/src/test/java/de/hilling/cdi/sampleapp/controller/RegistrationBeanTest.java
@@ -2,6 +2,8 @@ package de.hilling.cdi.sampleapp.controller;
 
 import de.hilling.cdi.sampleapp.ejb.RegistrationService;
 import de.hilling.junit.cdi.CdiUnitRunner;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -18,10 +20,20 @@ public class RegistrationBeanTest {
     @Mock
     private RegistrationService service;
 
-    @Test
-    public void register() {
+    @Before
+    public void setUp() {
         registrationBean.setAge(10);
         registrationBean.setName("Gunnar");
+    }
+
+    @Test
+    public void checkValues() {
+        Assert.assertEquals("Gunnar", registrationBean.getName());
+        Assert.assertEquals(10, registrationBean.getAge());
+    }
+
+    @Test
+    public void register() {
         registrationBean.register();
         Mockito.verify(service).register("Gunnar");
     }

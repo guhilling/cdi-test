dd253a91d816110b98f826bccc4bc4d3e22bdbdd
Gunnar Hilling <gunnar@hilling.de>
2015-10-20 14:21:07 +0200

    * Refactor boot process. * add tests.

--- cdi-test-core/src/test/java/de/hilling/junit/cdi/scope/ContextControlTest.java
+++ cdi-test-core/src/test/java/de/hilling/junit/cdi/scope/ContextControlTest.java
@@ -5,8 +5,12 @@ import de.hilling.junit.cdi.scopedbeans.ApplicationScopedBean;
 import de.hilling.junit.cdi.scopedbeans.RequestScopedBean;
 import de.hilling.junit.cdi.scopedbeans.ScopedBean;
 import de.hilling.junit.cdi.scopedbeans.SessionScopedBean;
+import de.hilling.junit.cdi.util.MavenVersion;
+import de.hilling.junit.cdi.util.MavenVersionResolver;
 import org.apache.deltaspike.cdise.api.ContextControl;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -19,6 +23,9 @@ import java.util.UUID;
 @RunWith(CdiUnitRunner.class)
 public class ContextControlTest {
 
+    private final MavenVersionResolver versionResolver = MavenVersionResolver.getInstance();
+    private final MavenVersion minimumWeldVersion = new MavenVersion(2, 2);
+
     @Inject
     private RequestScopedBean requestScopedBean;
     @Inject
@@ -30,12 +37,14 @@ public class ContextControlTest {
     private ContextControl contextControl;
 
     @Test
-    public void restartRequestn() {
+    public void restartRequest() {
         runTest(requestScopedBean, RequestScoped.class);
     }
 
     @Test
     public void restartApplication() {
+        MavenVersion actualVersion = versionResolver.getVersion("org.jboss.weld", "weld-api");
+        Assume.assumeThat(actualVersion, Matchers.greaterThan(minimumWeldVersion));
         runTest(applicationScopedBean, ApplicationScoped.class);
     }
 

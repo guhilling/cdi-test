7a3ad8db0bdace86b96b2464ee798292c1893264
Gunnar Hilling <gunnar@hilling.de>
2015-10-20 14:34:39 +0200

    * add tests.

--- integration-tests/test-jsf-war/src/test/java/de/hilling/cdi/sampleapp/ejb/RegistrationServiceUnitTest.java
+++ integration-tests/test-jsf-war/src/test/java/de/hilling/cdi/sampleapp/ejb/RegistrationServiceUnitTest.java
@@ -1,8 +1,10 @@
 package de.hilling.cdi.sampleapp.ejb;
 
+import de.hilling.cdi.sampleapp.UserRegistrationEntity;
 import de.hilling.junit.cdi.CdiUnitRunner;
 import de.hilling.junit.cdi.annotations.TestImplementation;
 import de.hilling.junit.cdi.jee.EntityManagerTestProducer;
+import org.hamcrest.core.Is;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -13,10 +15,12 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @RunWith(CdiUnitRunner.class)
 public class RegistrationServiceUnitTest {
 
+    public static final String NAME = "Gunnar";
     @Inject
     private RegistrationService registrationService;
 
@@ -27,24 +31,28 @@ public class RegistrationServiceUnitTest {
     @Produces
     @Named(EntityManagerTestProducer.PERSISTENCE_UNIT_PROPERTY)
     private String persistencUnit = "cdi-test-unit-eclipselink";
+    private List<UserRegistrationEntity> allUsers;
 
     private void assertDatabaseSize(int expectedSize) {
-        List list = entityManager.createQuery("select e from UserRegistrationEntity e").getResultList();
-        assertEquals(expectedSize, list.size());
+        allUsers = entityManager.createQuery("select e from UserRegistrationEntity e").getResultList();
+        assertEquals(expectedSize, allUsers.size());
     }
 
     @Test
     public void testSayHello() throws Exception {
         assertDatabaseSize(0);
-        registrationService.register("Gunnar");
+        registrationService.register(NAME);
         entityManager.flush();
         assertDatabaseSize(1);
+        UserRegistrationEntity entity = allUsers.get(0);
+        assertEquals("Hello " + NAME, entity.getUserName());
+        assertEquals(1, entity.getId());
     }
 
     @Test
     public void testSayHelloTwice() throws Exception {
         assertDatabaseSize(0);
-        registrationService.register("Gunnar");
+        registrationService.register(NAME);
         entityManager.flush();
         assertDatabaseSize(1);
     }

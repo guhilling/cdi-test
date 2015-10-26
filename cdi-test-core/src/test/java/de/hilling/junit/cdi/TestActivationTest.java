2a37d75db0b74269706d85f841caa3c07da01f2b
Gunnar Hilling <gunnar@hilling.de>
2015-10-26 23:03:31 +0100

    CDIT-16 - Generalize mocking feature for test implementations.

--- cdi-test-core/src/test/java/de/hilling/junit/cdi/TestActivationTest.java
+++ cdi-test-core/src/test/java/de/hilling/junit/cdi/TestActivationTest.java
@@ -2,6 +2,7 @@ package de.hilling.junit.cdi;
 
 import de.hilling.junit.cdi.beans.Person;
 import de.hilling.junit.cdi.service.BackendService;
+import de.hilling.junit.cdi.service.OverriddenService;
 import de.hilling.junit.cdi.service.TestActivatedOverridenService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,7 +13,9 @@ import javax.inject.Inject;
 @RunWith(CdiUnitRunner.class)
 public class TestActivationTest {
     @Inject
-    private TestActivatedOverridenService overridenService;
+    private TestActivatedOverridenService testOverride;
+    @Inject
+    private OverriddenService overriddenService;
     @Inject
     private BackendService backendService;
 
@@ -20,13 +23,30 @@ public class TestActivationTest {
     public void callTestActivatedService() {
         backendService.storePerson(new Person());
         backendService.storePerson(new Person());
-        Assert.assertEquals(2, overridenService.getInvocationCounter());
+        Assert.assertEquals(2, testOverride.getInvocationCounter());
     }
 
     @Test
     public void callTestActivatedServiceIndependently() {
         backendService.storePerson(new Person());
         backendService.storePerson(new Person());
-        Assert.assertEquals(2, overridenService.getInvocationCounter());
+        Assert.assertEquals(2, testOverride.getInvocationCounter());
+    }
+
+    @Test
+    public void callOverridenServiceDirectly() {
+        overriddenService.serviceMethod();
+        overriddenService.serviceMethod();
+        overriddenService.serviceMethod();
+        Assert.assertEquals(3, testOverride.getInvocationCounter());
+    }
+
+    @Test
+    public void callOverridenServiceMixed() {
+        overriddenService.serviceMethod();
+        overriddenService.serviceMethod();
+        overriddenService.serviceMethod();
+        backendService.storePerson(new Person());
+        Assert.assertEquals(4, testOverride.getInvocationCounter());
     }
 }

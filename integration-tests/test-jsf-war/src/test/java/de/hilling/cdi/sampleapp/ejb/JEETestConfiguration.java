bd818756d4bec40f840712e620719632e35d9875
Gunnar Hilling <gunnar@hilling.de>
2015-10-28 23:56:21 +0100

    Fix Scope (error during release)

--- integration-tests/test-jsf-war/src/test/java/de/hilling/cdi/sampleapp/ejb/JEETestConfiguration.java
+++ integration-tests/test-jsf-war/src/test/java/de/hilling/cdi/sampleapp/ejb/JEETestConfiguration.java
@@ -1,8 +1,10 @@
 package de.hilling.cdi.sampleapp.ejb;
 
 import de.hilling.junit.cdi.annotations.GlobalTestImplementation;
+import de.hilling.junit.cdi.scope.TestSuiteScoped;
 
 @GlobalTestImplementation
+@TestSuiteScoped
 public class JEETestConfiguration implements de.hilling.junit.cdi.jee.JEETestConfiguration {
     @Override
     public String getTestPersistenceUnitName() {

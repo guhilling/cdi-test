ce08027b78efce32d18118e82497d6231a544963
Gunnar Hilling <gunnar@hilling.de>
2018-03-28 11:11:30 +0200

    cleanup.

--- cdi-test-cucumber/src/test/java/toplevelpackage/AdditionStepDefinition.java
+++ cdi-test-cucumber/src/test/java/toplevelpackage/AdditionStepDefinition.java
@@ -8,10 +8,6 @@ import static org.junit.Assert.assertTrue;
 
 import javax.enterprise.context.ApplicationScoped;
 
-/**
- * Step definition without package.
- * author: fseemann on 13.04.2015.
- */
 @ApplicationScoped
 public class AdditionStepDefinition {
     Integer augend;
@@ -19,21 +15,18 @@ public class AdditionStepDefinition {
     Integer sum;
 
     @Given("^two integers with value 1$")
-    public void two_integers_with_value()
-    throws Throwable {
+    public void two_integers_with_value() {
         augend = 1;
         addend = 1;
     }
 
     @When("^those integers get added$")
-    public void those_integers_get_added()
-    throws Throwable {
+    public void those_integers_get_added() {
         sum = augend + addend;
     }
 
     @Then("^the result should be 2$")
-    public void the_result_should_be()
-    throws Throwable {
+    public void the_result_should_be() {
         assertTrue(sum == 2);
     }
 }

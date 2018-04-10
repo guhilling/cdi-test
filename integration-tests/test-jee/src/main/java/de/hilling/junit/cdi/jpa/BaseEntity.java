55fe42c527a0bddcc174924051608ad7032ee802
Gunnar Hilling <gunnar@hilling.de>
2018-04-10 20:50:32 +0200

    - generate pk.

--- integration-tests/test-jee/src/main/java/de/hilling/junit/cdi/jpa/BaseEntity.java
+++ integration-tests/test-jee/src/main/java/de/hilling/junit/cdi/jpa/BaseEntity.java
@@ -1,5 +1,7 @@
 package de.hilling.junit.cdi.jpa;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
@@ -7,6 +9,7 @@ import javax.persistence.Version;
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
     @Version

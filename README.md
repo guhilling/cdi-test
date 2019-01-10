d08cf36cfc2beb7c070bec377889405574232cbe
Gunnar Hilling <gunnar@hilling.de>
2019-01-10 12:43:53 +0100

    update integration with smallrye-config

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 ## LICENSE
 
- Copyright 2015 Gunnar Hilling
+ Copyright 2019 Gunnar Hilling
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
--- cdi-test-microprofile/src/main/java/de/hilling/junit/cdi/microprofile/TestConfigProducer.java
+++ cdi-test-microprofile/src/main/java/de/hilling/junit/cdi/microprofile/TestConfigProducer.java
@@ -2,25 +2,16 @@ package de.hilling.junit.cdi.microprofile;
 
 import de.hilling.junit.cdi.annotations.GlobalTestImplementation;
 import de.hilling.junit.cdi.scope.TestSuiteScoped;
-import io.smallrye.config.SmallRyeConfig;
-import io.smallrye.config.StringUtil;
+import io.smallrye.config.inject.ConfigProducerUtil;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
-import javax.enterprise.inject.spi.AnnotatedMember;
-import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Inject;
 import java.io.Serializable;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @TestSuiteScoped
 public class TestConfigProducer implements Serializable {
@@ -28,39 +19,12 @@ public class TestConfigProducer implements Serializable {
     @Inject
     private Config config;
 
-    static String getConfigKey(InjectionPoint ip, ConfigProperty configProperty) {
-        String key = configProperty.name();
-        if (!key.trim()
-                .isEmpty()) {
-            return key;
-        }
-        if (ip.getAnnotated() instanceof AnnotatedMember) {
-            AnnotatedMember member = (AnnotatedMember) ip.getAnnotated();
-            AnnotatedType declaringType = member.getDeclaringType();
-            if (declaringType != null) {
-                String[] parts = declaringType.getJavaClass()
-                                              .getCanonicalName()
-                                              .split("\\.");
-                StringBuilder sb = new StringBuilder(parts[0]);
-                for (int i = 1; i < parts.length; i++) {
-                    sb.append(".")
-                      .append(parts[i]);
-                }
-                sb.append(".")
-                  .append(member.getJavaMember()
-                                .getName());
-                return sb.toString();
-            }
-        }
-        throw new IllegalStateException("Could not find default name for @ConfigProperty InjectionPoint " + ip);
-    }
-
     @GlobalTestImplementation
     @Dependent
     @Produces
     @ConfigProperty
     String produceStringConfigProperty(InjectionPoint ip) {
-        return getValue(ip, String.class);
+        return ConfigProducerUtil.getValue(ip, String.class, config);
     }
 
     @GlobalTestImplementation
@@ -68,7 +32,7 @@ public class TestConfigProducer implements Serializable {
     @Produces
     @ConfigProperty
     Long getLongValue(InjectionPoint ip) {
-        return getValue(ip, Long.class);
+        return ConfigProducerUtil.getValue(ip, Long.class, config);
     }
 
     @GlobalTestImplementation
@@ -76,7 +40,7 @@ public class TestConfigProducer implements Serializable {
     @Produces
     @ConfigProperty
     Integer getIntegerValue(InjectionPoint ip) {
-        return getValue(ip, Integer.class);
+        return ConfigProducerUtil.getValue(ip, Integer.class, config);
     }
 
     @GlobalTestImplementation
@@ -84,7 +48,7 @@ public class TestConfigProducer implements Serializable {
     @Produces
     @ConfigProperty
     Float produceFloatConfigProperty(InjectionPoint ip) {
-        return getValue(ip, Float.class);
+        return ConfigProducerUtil.getValue(ip, Float.class, config);
     }
 
     @GlobalTestImplementation
@@ -92,7 +56,7 @@ public class TestConfigProducer implements Serializable {
     @Produces
     @ConfigProperty
     Double produceDoubleConfigProperty(InjectionPoint ip) {
-        return getValue(ip, Double.class);
+        return ConfigProducerUtil.getValue(ip, Double.class, config);
     }
 
     @GlobalTestImplementation
@@ -100,7 +64,7 @@ public class TestConfigProducer implements Serializable {
     @Produces
     @ConfigProperty
     Boolean produceBooleanConfigProperty(InjectionPoint ip) {
-        return getValue(ip, Boolean.class);
+        return ConfigProducerUtil.getValue(ip, Boolean.class, config);
     }
 
     @GlobalTestImplementation
@@ -108,18 +72,7 @@ public class TestConfigProducer implements Serializable {
     @Produces
     @ConfigProperty
     <T> Optional<T> produceOptionalConfigValue(InjectionPoint injectionPoint) {
-        Type type = injectionPoint.getAnnotated()
-                                  .getBaseType();
-        final Class<T> valueType;
-        if (type instanceof ParameterizedType) {
-            ParameterizedType parameterizedType = (ParameterizedType) type;
-
-            Type[] typeArguments = parameterizedType.getActualTypeArguments();
-            valueType = unwrapType(typeArguments[0]);
-        } else {
-            valueType = (Class<T>) String.class;
-        }
-        return Optional.ofNullable(getValue(injectionPoint, valueType));
+        return ConfigProducerUtil.optionalConfigValue(injectionPoint, config);
     }
 
     @GlobalTestImplementation
@@ -127,25 +80,7 @@ public class TestConfigProducer implements Serializable {
     @Produces
     @ConfigProperty
     <T> Set<T> producesSetConfigPropery(InjectionPoint ip) {
-        Type type = ip.getAnnotated()
-                      .getBaseType();
-        final Class<T> valueType;
-        if (type instanceof ParameterizedType) {
-            ParameterizedType parameterizedType = (ParameterizedType) type;
-
-            Type[] typeArguments = parameterizedType.getActualTypeArguments();
-            valueType = unwrapType(typeArguments[0]);
-        } else {
-            valueType = (Class<T>) String.class;
-        }
-        HashSet<T> s = new HashSet<>();
-        String stringValue = getValue(ip, String.class);
-        String[] split = StringUtil.split(stringValue);
-        for (int i = 0; i < split.length; i++) {
-            T item = ((SmallRyeConfig) config).convert(split[i], valueType);
-            s.add(item);
-        }
-        return s;
+        return ConfigProducerUtil.collectionConfigProperty(ip, config, new HashSet<>());
     }
 
     @GlobalTestImplementation
@@ -153,58 +88,7 @@ public class TestConfigProducer implements Serializable {
     @Produces
     @ConfigProperty
     <T> List<T> producesListConfigPropery(InjectionPoint ip) {
-        return null;
-    }
-
-    private <T> Class<T> unwrapType(Type type) {
-        if (type instanceof ParameterizedType) {
-            type = ((ParameterizedType) type).getRawType();
-        }
-        return (Class<T>) type;
-    }
-
-    private <T> T getValue
-            (InjectionPoint injectionPoint, Class<T> target) {
-        String name = getName(injectionPoint);
-        try {
-            if (name == null) {
-                return null;
-            }
-            Optional<T> optionalValue = config.getOptionalValue(name, target);
-            if (optionalValue.isPresent()) {
-                return optionalValue.get();
-            } else {
-                String defaultValue = getDefaultValue(injectionPoint);
-                if (defaultValue != null && !defaultValue.equals(ConfigProperty.UNCONFIGURED_VALUE)) {
-                    return ((SmallRyeConfig) config).convert(defaultValue, target);
-                } else {
-                    return null;
-                }
-            }
-        } catch (RuntimeException e) {
-            return null;
-        }
-    }
-
-    private String getName(InjectionPoint injectionPoint) {
-        for (Annotation qualifier : injectionPoint.getQualifiers()) {
-            if (qualifier.annotationType()
-                         .equals(ConfigProperty.class)) {
-                ConfigProperty configProperty = ((ConfigProperty) qualifier);
-                return getConfigKey(injectionPoint, configProperty);
-            }
-        }
-        return null;
-    }
-
-    private String getDefaultValue(InjectionPoint injectionPoint) {
-        for (Annotation qualifier : injectionPoint.getQualifiers()) {
-            if (qualifier.annotationType()
-                         .equals(ConfigProperty.class)) {
-                return ((ConfigProperty) qualifier).defaultValue();
-            }
-        }
-        return null;
+        return ConfigProducerUtil.collectionConfigProperty(ip, config, new ArrayList<T>());
     }
 
 }

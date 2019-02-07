26a67825ffa7282c988f10d9e101dcd68bb21fc6
Gunnar Hilling <gunnar@hilling.de>
2019-02-07 15:43:11 +0100

    use mockito extension

--- cdi-test-core/src/test/java/de/hilling/junit/cdi/ActivatePartialAlternativeForRegularBeanTest.java
+++ cdi-test-core/src/test/java/de/hilling/junit/cdi/ActivatePartialAlternativeForRegularBeanTest.java
@@ -1,16 +1,18 @@
 package de.hilling.junit.cdi;
 
-import javax.inject.Inject;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
 import de.hilling.junit.cdi.beans.Person;
 import de.hilling.junit.cdi.service.BackendServiceTestPartialImplementation;
 import de.hilling.junit.cdi.service.SampleService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.inject.Inject;
 
 @ExtendWith(CdiTestJunitExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class ActivatePartialAlternativeForRegularBeanTest {
+
     @Inject
     private SampleService sampleService;
     @Inject

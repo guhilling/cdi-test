1ac8571bd0f64ccd3f7cf3b018f50df89fc97773
Gunnar Hilling <gunnar@hilling.de>
2019-02-18 11:15:17 +0100

    - update to junit-5. - remove cucumber support from this repo due to missing junit 5 support. - see https://github.com/guhilling/cdi-test-cucumber.git - update dependencies.

--- cdi-test-cucumber/src/test/java/TestRunner.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-
-import cucumber.api.CucumberOptions;
-import de.hilling.junit.cdi.cucumber.CucumberCdi;
-
-@RunWith(CucumberCdi.class)
-@CucumberOptions(features = {"src/test/resources/de/hilling/junit/cdi/cucumber"})
-public class TestRunner {
-}

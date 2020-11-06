2204023727fa9cf6ebca4701c19a3cc30caf571f
Gunnar Hilling <gunnar@hilling.de>
2020-11-06 20:10:32 +0100

    - mainly testing for new Singleton support.

---
+++ microprofile-integration-tests/src/main/java/de/hilling/junit/cdi/microprofile/EjbController.java
@@ -0,0 +1,51 @@
+package de.hilling.junit.cdi.microprofile;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+
+@Singleton
+public class EjbController {
+
+    @Inject
+    @ConfigProperty(name = "some.string.property")
+    private String stringProperty;
+
+    @Inject
+    @ConfigProperty(name = "some.integer.property")
+    private Integer intProperty;
+
+    @Inject
+    @ConfigProperty(name = "some.boolean.property")
+    private Boolean boolProperty;
+
+    @Inject
+    @ConfigProperty(name = "some.long.property")
+    private Long longProperty;
+
+    @Inject
+    @ConfigProperty(name = "some.horse.property")
+    private Horse horseProperty;
+
+    public String getStringProperty() {
+        return stringProperty;
+    }
+
+    public int getIntegerProperty() {
+        return intProperty;
+    }
+
+    public boolean getBoolProperty() {
+        return boolProperty;
+    }
+
+    public long getLongProperty() {
+        return longProperty;
+    }
+
+    public String getHorseProperty() {
+        return horseProperty.getName();
+    }
+
+}

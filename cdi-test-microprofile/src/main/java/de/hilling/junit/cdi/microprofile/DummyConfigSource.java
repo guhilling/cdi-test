73e3c0e9212849dfa0edafa7644fcf0a9aaaf4f6
Gunnar Hilling <gunnar@hilling.de>
2020-12-07 08:48:38 +0100

    - config

--- cdi-test-microprofile/src/main/java/de/hilling/junit/cdi/microprofile/DummyConfigSource.java
+++ cdi-test-microprofile/src/main/java/de/hilling/junit/cdi/microprofile/DummyConfigSource.java
@@ -3,8 +3,10 @@ package de.hilling.junit.cdi.microprofile;
 import de.hilling.junit.cdi.scope.TestSuiteScoped;
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Prevent any problems with non-resolved global properties.
@@ -15,6 +17,11 @@ public class DummyConfigSource implements ConfigSource {
     @Override
     public Map<String, String> getProperties() {
         return new HashMap<>();
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return Collections.singleton("dummy");
     }
 
     @Override

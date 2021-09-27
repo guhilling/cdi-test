e14290bc3279ee09da550fae9eb529aa01982003
Gunnar Hilling <gunnar@hilling.de>
2021-09-27 17:00:43 +0200

    initial version.

--- cdi-test-jee/src/test/java/de/hilling/junit/cdi/jee/TestEntityListener.java
+++ cdi-test-jee/src/test/java/de/hilling/junit/cdi/jee/TestEntityListener.java
@@ -3,6 +3,8 @@ package de.hilling.junit.cdi.jee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.PrePersist;
 import javax.persistence.PreRemove;

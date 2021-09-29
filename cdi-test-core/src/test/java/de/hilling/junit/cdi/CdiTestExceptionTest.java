b0089c73341f3f6a8a9ad2fb990bdeaf6a47eef3
Gunnar Hilling <gunnar@hilling.de>
2021-09-29 11:09:52 +0200

    - configure sonar

--- cdi-test-core/src/test/java/de/hilling/junit/cdi/CdiTestExceptionTest.java
+++ cdi-test-core/src/test/java/de/hilling/junit/cdi/CdiTestExceptionTest.java
@@ -1,12 +1,15 @@
 package de.hilling.junit.cdi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CdiTestExceptionTest {
 
     @Test
     void createExceptions() {
-        new CdiTestException("msg");
-        new CdiTestException("msg", new RuntimeException());
+        CdiTestException plainException = new CdiTestException("msg");
+        Assertions.assertNull(plainException.getCause());
+        CdiTestException cdiTestException = new CdiTestException("msg", new RuntimeException("cause"));
+        Assertions.assertEquals("cause", cdiTestException.getCause().getMessage());
     }
 }

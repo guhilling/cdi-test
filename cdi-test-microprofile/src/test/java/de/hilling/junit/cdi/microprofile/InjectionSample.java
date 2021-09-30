7ef9d8ae06da5f1a2b25a7c2b4f8fcdcb9c1ac0b
Gunnar Hilling <gunnar@hilling.de>
2021-09-30 14:00:21 +0200

    - add tests

--- cdi-test-microprofile/src/test/java/de/hilling/junit/cdi/microprofile/InjectionSample.java
+++ cdi-test-microprofile/src/test/java/de/hilling/junit/cdi/microprofile/InjectionSample.java
@@ -14,7 +14,40 @@ public class InjectionSample {
     @Inject
     private String someProperty;
 
+    @Inject
+    @ConfigProperty(name = "some.string.property", defaultValue = "default")
+    private String stringProperty;
+
+    @Inject
+    @ConfigProperty(name = "some.integer.property", defaultValue = "1")
+    private Integer intProperty;
+
+    @Inject
+    @ConfigProperty(name = "some.boolean.property", defaultValue = "true")
+    private Boolean boolProperty;
+
+    @Inject
+    @ConfigProperty(name = "some.long.property", defaultValue = "1")
+    private Long longProperty;
+
+
     public String getSomeProperty() {
         return someProperty;
+    }
+
+    public String getStringProperty() {
+        return stringProperty;
+    }
+
+    public Integer getIntProperty() {
+        return intProperty;
+    }
+
+    public Boolean getBoolProperty() {
+        return boolProperty;
+    }
+
+    public Long getLongProperty() {
+        return longProperty;
     }
 }

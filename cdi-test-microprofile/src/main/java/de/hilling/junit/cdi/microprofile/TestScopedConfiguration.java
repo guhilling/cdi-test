23f52f4bf8ec180e31f43bbae3d22b2effcb08b5
Gunnar Hilling <gunnar@hilling.de>
2021-10-03 20:48:18 +0200

    - add more tests.

--- cdi-test-microprofile/src/main/java/de/hilling/junit/cdi/microprofile/TestScopedConfiguration.java
+++ cdi-test-microprofile/src/main/java/de/hilling/junit/cdi/microprofile/TestScopedConfiguration.java
@@ -2,8 +2,10 @@ package de.hilling.junit.cdi.microprofile;
 
 import de.hilling.junit.cdi.annotations.GlobalTestImplementation;
 import de.hilling.junit.cdi.scope.TestScoped;
+
 import io.smallrye.config.PropertiesConfigSourceProvider;
 import io.smallrye.config.SmallRyeConfigBuilder;
+
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
@@ -12,6 +14,7 @@ import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
@@ -19,8 +22,10 @@ import java.util.Set;
 @Priority(10000)
 @TestScoped
 public class TestScopedConfiguration implements ConfigSource {
+    public static final String TEST_CONFIGURATION_NAME = "TEST_CASE_MICROPROFILE_CONFIGURATION";
+
     private static final String META_INF_MICROPROFILE_CONFIG_PROPERTIES = "META-INF/microprofile-config.properties";
-    private static final String WEB_INF_MICROPROFILE_CONFIG_PROPERTIES = "WEB-INF/classes/META-INF/microprofile-config.properties";
+    private static final String WEB_INF_MICROPROFILE_CONFIG_PROPERTIES  = "WEB-INF/classes/META-INF/microprofile-config.properties";
 
     @GlobalTestImplementation
     @Dependent
@@ -37,17 +42,19 @@ public class TestScopedConfiguration implements ConfigSource {
         testProperties = properties.getProperties();
 
         final ClassLoader classLoader = Thread.currentThread()
-                                              .getContextClassLoader();
+                .getContextClassLoader();
 
         ArrayList<ConfigSource> defaultSources = new ArrayList<>();
-        defaultSources.addAll(new PropertiesConfigSourceProvider(META_INF_MICROPROFILE_CONFIG_PROPERTIES, true, classLoader).getConfigSources(classLoader));
-        defaultSources.addAll(new PropertiesConfigSourceProvider(WEB_INF_MICROPROFILE_CONFIG_PROPERTIES, true, classLoader).getConfigSources(classLoader));
+        defaultSources.addAll(new PropertiesConfigSourceProvider(META_INF_MICROPROFILE_CONFIG_PROPERTIES, true, classLoader).getConfigSources(
+                classLoader));
+        defaultSources.addAll(new PropertiesConfigSourceProvider(WEB_INF_MICROPROFILE_CONFIG_PROPERTIES, true, classLoader).getConfigSources(
+                classLoader));
         defaultSources.add(this);
 
         testConfig = new SmallRyeConfigBuilder().addDefaultSources()
-                                                .withSources(defaultSources.toArray(new ConfigSource[0]))
-                                                .addDiscoveredConverters()
-                                                .build();
+                .withSources(defaultSources.toArray(new ConfigSource[0]))
+                .addDiscoveredConverters()
+                .build();
     }
 
     @Override
@@ -67,7 +74,7 @@ public class TestScopedConfiguration implements ConfigSource {
 
     @Override
     public String getName() {
-        return "per test mp configuration";
+        return TEST_CONFIGURATION_NAME;
     }
 
 }
---
+++ cdi-test-microprofile/src/test/java/de/hilling/junit/cdi/microprofile/TestScopedConfigurationTest.java
@@ -0,0 +1,41 @@
+package de.hilling.junit.cdi.microprofile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import de.hilling.junit.cdi.CdiTestJunitExtension;
+
+@ExtendWith(CdiTestJunitExtension.class)
+@ConfigPropertyValue(name = TestScopedConfigurationTest.TEST_PROPERTY, value = TestScopedConfigurationTest.TEST_PROPERTY_VALUE)
+class TestScopedConfigurationTest {
+
+    public static final String TEST_PROPERTY = "demoTestProperty";
+    public static final String TEST_PROPERTY_VALUE = "Hello, Test!";
+
+    @Inject
+    private TestScopedConfiguration configuration;
+
+    @Test
+    void accessProperties() {
+        assertEquals(TEST_PROPERTY_VALUE, configuration.getProperties().get(TEST_PROPERTY));
+    }
+
+    @Test
+    void propertyNames() {
+        assertEquals(1, configuration.getPropertyNames().size());
+    }
+
+    @Test
+    void accessProperty() {
+        assertEquals(TEST_PROPERTY_VALUE, configuration.getValue(TEST_PROPERTY));
+    }
+
+    @Test
+    void checkName() {
+        assertEquals(TestScopedConfiguration.TEST_CONFIGURATION_NAME, configuration.getName());
+    }
+}

29cf9e2e5e7fdc3fe658bbf176d0ebf8ddde0a4e
Gunnar Hilling <gunnar@hilling.de>
2022-02-03 09:55:17 +0100

    - Add RequestScoped Injection also for JEE.

---
+++ integration-tests/test-jee/src/test/java/de/hilling/junit/cdi/jpa/RequestScopedUserServiceTest.java
@@ -0,0 +1,22 @@
+package de.hilling.junit.cdi.jpa;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import de.hilling.junit.cdi.CdiTestJunitExtension;
+
+@ExtendWith(CdiTestJunitExtension.class)
+public class RequestScopedUserServiceTest {
+
+    @Inject
+    private RequestScopedUserService userService;
+    @Inject
+    private TestUtils testUtils;
+
+    @Test
+    public void addUser() {
+        userService.addUser(testUtils.createGunnar());
+    }
+}

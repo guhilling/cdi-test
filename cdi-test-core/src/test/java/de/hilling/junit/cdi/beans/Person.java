cf4deaa5311f140226e7d04f000c3e17b73fb3f1
Gunnar Hilling <gunnar@hilling.de>
2022-03-11 10:10:53 +0100

    Demo Parameterized Test

--- cdi-test-core/src/test/java/de/hilling/junit/cdi/beans/Person.java
+++ cdi-test-core/src/test/java/de/hilling/junit/cdi/beans/Person.java
@@ -3,23 +3,18 @@ package de.hilling.junit.cdi.beans;
 public class Person {
     private String name;
 
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public static class Address {
-        private String description;
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
     }
 }

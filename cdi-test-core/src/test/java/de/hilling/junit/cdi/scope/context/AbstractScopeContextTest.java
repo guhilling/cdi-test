c829a3251204339acf761204e531301b202b6a19
Gunnar Hilling <gunnar@hilling.de>
2022-07-27 15:50:18 +0200

    - fix test.

--- cdi-test-core/src/test/java/de/hilling/junit/cdi/scope/context/AbstractScopeContextTest.java
+++ cdi-test-core/src/test/java/de/hilling/junit/cdi/scope/context/AbstractScopeContextTest.java
@@ -24,7 +24,7 @@ class AbstractScopeContextTest {
     private static Map<Type, UUID> uuidMap;
 
     @BeforeAll
-    private static void reset() {
+    static void reset() {
         uuidMap = new HashMap<>();
     }
 
